2935cdbd0d21fcbea1cc1d4250c6b90c35e5aff4
MeilakhSergey <meylakhsergey@gmail.com>
2018-11-18 19:00:16 +0300

    Start

---
+++ src/MiddlePermutation.java
@@ -0,0 +1,14 @@
+public class MiddlePermutation {
+    public static void main(String[] args) {
+        System.out.println(findMidPerm("abc"));
+        System.out.println(findMidPerm("abcd"));
+        System.out.println(findMidPerm("abcdx"));
+        System.out.println(findMidPerm("abcdxg"));
+        System.out.println(findMidPerm("abcdxgz"));
+    }
+
+    public static String findMidPerm(String strng) {
+        // your code here!
+        return "";
+    }
+}
