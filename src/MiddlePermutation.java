554384ba4d7101ab90e0d5dbf3e395bfc0e3b388
MeilakhSergey <meylakhsergey@gmail.com>
2018-11-18 21:13:45 +0300

    Changed long fact to BigInteger

--- src/MiddlePermutation.java
+++ src/MiddlePermutation.java
@@ -1,18 +1,22 @@
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class MiddlePermutation {
     private static StringBuilder result;
     private static boolean isFound;
-    private static long fact;
+    private static BigInteger fact;
     private static int n;
     public static void main(String[] args) {
+        System.out.println(findMidPerm("ab"));
         System.out.println(findMidPerm("abc"));
         System.out.println(findMidPerm("abcd"));
         System.out.println(findMidPerm("abcdx"));
         System.out.println(findMidPerm("abcdxg"));
         System.out.println(findMidPerm("abcdxgz"));
         System.out.println(findMidPerm("obwetzmafglcsxvhu"));
+        System.out.println(findMidPerm("qutzrvjkmdweflyha"));
+        System.out.println(findMidPerm("pxyomsjlucdrawvzqbitfk"));
     }
 
     public static String findMidPerm(String strng) {
@@ -21,8 +25,9 @@ public class MiddlePermutation {
         strng = new String(orderArray);
         result = new StringBuilder();
         isFound = false;
-        fact = factorial(orderArray.length) / 2;
         n = orderArray.length;
+        fact = factorial(new BigInteger(Integer.toString(n)));
+        fact = fact.divide(new BigInteger("2"));
         findMidPerm(strng, n);
         return result.toString();
     }
@@ -32,13 +37,13 @@ public class MiddlePermutation {
             return;
         else {
             int i;
-            if (fact % (fact * 2 / n) != 0) {
-                i = (int) (fact / (fact * 2 / n));
+            if (fact.remainder(fact.multiply(new BigInteger("2")).divide(new BigInteger(Integer.toString(n)))).compareTo(new BigInteger("0")) != 0) {           //if (fact % (fact * 2 / n) != 0) {
+                i = fact.divide(fact.multiply(new BigInteger("2")).divide(new BigInteger(Integer.toString(n)))).intValue();   //(int) (fact / (fact * 2 / n))
                 result.append(str.charAt(i));
-                fact %= (fact * 2 / n);
+                fact = fact.remainder(fact.multiply(new BigInteger("2")).divide(new BigInteger(Integer.toString(n))));        //fact %= (fact * 2 / n);
                 n--;
             } else {
-                i = (int) (fact / (fact * 2 / n)) - 1;
+                i = fact.divide(fact.multiply(new BigInteger("2")).divide(new BigInteger(Integer.toString(n)))).intValue();     //i = (int) (fact / (fact * 2 / n)) - 1;
                 result.append(str.charAt(i));
                 String temp = str.substring(0, i) + str.substring(i + 1);
                 for (int j = temp.length() - 1; j >=0; j--) {
@@ -50,10 +55,10 @@ public class MiddlePermutation {
         }
     }
 
-    private static long factorial(long x) {
-        if (x == 1)
-            return 1;
+    private static BigInteger factorial(BigInteger x) {
+        if (x.intValue() == 1)
+            return new BigInteger("1");
         else
-            return x * factorial(x - 1);
+            return x.multiply(factorial(x.subtract(new BigInteger("1"))).negate());
     }
 }
