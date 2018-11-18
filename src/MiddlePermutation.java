7f6f32db56f0aaabd2f4cd23f37453a1b4454602
MeilakhSergey <meylakhsergey@gmail.com>
2018-11-18 19:31:30 +0300

    It works!

--- src/MiddlePermutation.java
+++ src/MiddlePermutation.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+
 public class MiddlePermutation {
+    static String result;
+    static long fact;
+    static long count;
     public static void main(String[] args) {
         System.out.println(findMidPerm("abc"));
         System.out.println(findMidPerm("abcd"));
@@ -8,7 +13,31 @@ public class MiddlePermutation {
     }
 
     public static String findMidPerm(String strng) {
-        // your code here!
-        return "";
+        char[] orderArray = strng.toCharArray();
+        Arrays.sort(orderArray);
+        strng = new String(orderArray);
+        fact = factorial(orderArray.length);
+        count = 0;
+        findMidPerm("", strng);
+        return result;
+    }
+    private static void findMidPerm(String prefix, String str) {
+        int n = str.length();
+        if (n == 0)
+            count++;
+        else
+            for (int i = 0; i < n; i++) {
+                findMidPerm(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1));
+                if (count == fact / 2) break;
+            }
+        if (count == fact / 2 && n == 0) {
+            result = prefix;
+        }
+    }
+    private static long factorial(long x) {
+        if (x == 1)
+            return 1;
+        else
+            return x * factorial(x - 1);
     }
 }
